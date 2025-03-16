04a676725987ec15fa55d5f54d95b592baa18b2b
AprilQx <xxq_010223@163.com>
2025-03-16 12:08:08 +0000

    upload the file for csd3

---
+++ training_scripts/finetuning.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=lora_finetuning        
+#SBATCH --output=logs/output_%j.log     
+#SBATCH --error=logs/error_%j.log       
+#SBATCH -p ampere  
+#SBATCH --account=TRAINING-GPU
+#SBATCH --gres=gpu:1 
+#SBATCH --nodes=1                       
+#SBATCH --ntasks=1                       
+#SBATCH --cpus-per-task=4               
+#SBATCH --mem=32G                        
+#SBATCH --time=04:00:00
+#SBATCH --mail-user=xx823@cam.ac.uk
+#SBATCH --mail-type=ALL
+
+set -e  
+module purge
+module load python/3.11.0-icl
+
+mkdir -p logs
+
+
+source .venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Debug Python environment
+echo "Python version: $(python --version)"
+echo "Python path: $(which python)"
+python -c "import torch; print('Torch Loaded:', torch.__version__)"
+python -c "import torch; print('CUDA Available:', torch.cuda.is_available())"
+
+# Run script
+python experiments/finetune_q_k_r_8.py
